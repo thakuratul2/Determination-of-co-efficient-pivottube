3f687bd740c954f638b70b65982845bd1165104d
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-13 19:10:36 +0530

    complete

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -22,31 +22,28 @@ function changeImage() {
   });
 }
 function datasheet1(){
- 
-  document.getElementById("sno").innerHTML = "1";
-  document.getElementById("h1").innerHTML = "0.11";
-  document.getElementById("h2").innerHTML = "0.107";
-  document.getElementById("h").innerHTML = "0.003";
-  document.getElementById("t").innerHTML = "10";
-  document.getElementById("sno2").innerHTML = "2";
-  document.getElementById("h11").innerHTML = "0.137";
-  document.getElementById("h22").innerHTML = "0.13";
-  document.getElementById("hh").innerHTML = "0.007";
-  document.getElementById("t2").innerHTML = "7";
-  document.getElementById("snoa").innerHTML = "1";
+
   document.getElementById("h1a").innerHTML = "0.11";
   document.getElementById("h2a").innerHTML = "0.107";
   document.getElementById("ha").innerHTML = "0.003";
   document.getElementById("ta").innerHTML = "10";
-  document.getElementById("snob").innerHTML = "2";
+  document.getElementById("r1").innerHTML = "0";
+  document.getElementById("r2").innerHTML = "1.57";
+  document.getElementById("va").innerHTML = "0.01";
+  document.getElementById("vth").innerHTML = "0.2426";
+  document.getElementById("qa").innerHTML = "3.14 X 10^-6";
+ 
   document.getElementById("h1b").innerHTML = "0.137";
   document.getElementById("h2b").innerHTML = "0.13";
   document.getElementById("hb").innerHTML = "0.007";
   document.getElementById("tb").innerHTML = "7";
-  document.getElementById("va").innerHTML = "0.01";
-  document.getElementById("vth").innerHTML = "0.2426";
+  document.getElementById("qb").innerHTML = "4.485 X 10^-6";
   document.getElementById("vaa").innerHTML = "0.014";
   document.getElementById("vthh").innerHTML = "0.3706";
+ 
+  
+  document.getElementById("r1b").innerHTML = "0";
+  document.getElementById("r2b").innerHTML = "2.24";
 }
 
 
